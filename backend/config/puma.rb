04548fc8e39847e6d446f05e5da77c7c77b5f2f0
Romuald Jarno <romuald.jarno@gmail.com>
2025-07-22 12:01:15 +0200

    update keys

--- backend/config/puma.rb
+++ backend/config/puma.rb
@@ -25,6 +25,8 @@ threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 port ENV.fetch("PORT", 3000)
+environment ENV.fetch("RAILS_ENV") { "development" }
+workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
