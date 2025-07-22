178fa037e8ffad6ff4c51a8f7f35a4223872160d
Romuald Jarno <romuald.jarno@gmail.com>
2025-07-22 22:38:49 +0200

    test

--- frontend/src/app/dashboard/index.tsx
+++ frontend/src/app/dashboard/index.tsx
@@ -13,7 +13,7 @@ export const Dashboard = () => {
       <AppSidebar variant="floating" />
       <SidebarInset>
         <SiteHeader />
-        <div className="flex flex-1 flex-col bg-red-500">
+        <div className="flex flex-1 flex-col">
           <div className="@container/main flex flex-1 flex-col gap-2">
             <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
               <SectionCards />
