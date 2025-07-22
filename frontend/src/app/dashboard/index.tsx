04d74465174ebe16533278d35b62a4714bb2014f
Romuald Jarno <romuald.jarno@gmail.com>
2025-07-22 22:26:13 +0200

    fix imports

--- frontend/src/app/dashboard/index.tsx
+++ frontend/src/app/dashboard/index.tsx
@@ -10,7 +10,7 @@ import data from "./data.json";
 export const Dashboard = () => {
   return (
     <SidebarProvider>
-      <AppSidebar variant="inset" />
+      <AppSidebar variant="floating" />
       <SidebarInset>
         <SiteHeader />
         <div className="flex flex-1 flex-col">
