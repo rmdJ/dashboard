ca90f6932936871083cebc5cdef551e39b4ae603
Romuald Jarno <romuald.jarno@gmail.com>
2025-08-18 09:18:36 +0200

    update portfolio

--- frontend/src/assets/constants/crypto.ts
+++ frontend/src/assets/constants/crypto.ts
@@ -3,11 +3,11 @@ export const portfolio: {
   quantity: number;
   ath: number;
 }[] = [
-  { name: "ETH", quantity: 1.4, ath: 4878.26 },
-  { name: "SOL", quantity: 6.2801, ath: 263.21 },
-  { name: "USDC", quantity: 289, ath: 1 },
-  { name: "NEAR", quantity: 70.94, ath: 20.44 },
-  { name: "BNB", quantity: 0.1234, ath: 717.48 },
+  { name: "ETH", quantity: 1.2104, ath: 4878.26 },
+  { name: "SOL", quantity: 9.1421, ath: 263.21 },
+  { name: "USDC", quantity: 0, ath: 1 },
+  { name: "NEAR", quantity: 401.79, ath: 20.44 },
+  { name: "BNB", quantity: 0.1261, ath: 717.48 },
   { name: "DOT", quantity: 16.56, ath: 54.98 },
   { name: "MATIC", quantity: 186.68, ath: 2.92 },
   { name: "ADA", quantity: 50, ath: 3.09 },
